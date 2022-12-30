8c21bba8371b726b8258aaaff01276f05eeb29c9
bgora <bartlomiej.gora@infobip.com>
2022-12-30 14:15:39 +0100

    Adapter in progress

--- src/main/java/com/github/bgora/CarDealerFacade.java
+++ src/main/java/com/github/bgora/CarDealerFacade.java
@@ -3,16 +3,13 @@ package com.github.bgora;
 import com.github.bgora.american.AmericanCar;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class CarDealerFacade {
 
     private final Map<String, AmericanCar> americanCarMap;
-    private final Map<String, Car> carMap;
 
     public CarDealerFacade() {
         americanCarMap = new HashMap<>();
-        carMap = new HashMap<>();
     }
 
     public void importFromUSA(final AmericanCar americanCar) {
@@ -24,18 +21,10 @@ public class CarDealerFacade {
     }
 
     public Car sellCar(final String name) {
-        var oldCar = americanCarMap.get(name);
-        var newSpeed = oldCar.maxSpeed() * 1.609344;
-        var newCar = new Car(oldCar.name(), oldCar.horsePower(), (int) newSpeed);
-        return newCar;
+        return null;
     }
 
     public List<Car> sellCars(final List<String> names) {
-        return carMap.entrySet()
-                     .stream()
-                     .filter(stringCarEntry -> names.contains(stringCarEntry.getKey()))
-                     .map(stringCarEntry -> stringCarEntry.getValue())
-                     .collect(
-                             Collectors.toList());
+        return null;
     }
 }
